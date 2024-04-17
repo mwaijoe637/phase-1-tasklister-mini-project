eabca56716c0494f3a24d75aa7fc3b1f9abb019f
joe <brakejoe9@gmail.com>
2024-04-17 13:11:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.addEventListener("click", function() {
+      taskItem.remove();
+    });
+
+    taskItem.appendChild(deleteButton);
+
+    const tasksList = document.getElementById("tasks");
+    tasksList.appendChild(taskItem);
+
+    taskInput.value = "";
+    taskInput.focus();
+  });
 });
